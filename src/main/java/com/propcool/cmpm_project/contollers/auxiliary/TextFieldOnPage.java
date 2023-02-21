24725a152a8fa43e7430b500ef93d92a7d3cd9f2
Exarkun1 <propcool56@gmail.com>
2023-02-21 16:35:43 +0400

    Alpha 1.0.2

--- src/main/java/com/propcool/cmpm_project/contollers/auxiliary/TextFieldOnPage.java
+++ src/main/java/com/propcool/cmpm_project/contollers/auxiliary/TextFieldOnPage.java
@@ -10,39 +10,42 @@ public class TextFieldOnPage extends TextField {
     public TextFieldOnPage(String text, MainController controller){
         super(text);
         setFont(new Font(25));
-        setOnKeyReleased(keyEvent -> {
-            String textOfField = getText();
-            NamedFunction nf = Elements.builder.building(textOfField);
-            if(nf == null || (Elements.functions.containsKey(nf.getName()) && !nf.getName().equals(functionName))
-                    || (Elements.parameters.containsKey(nf.getName())
-            )) {
-                Elements.functions.remove(functionName);
-                Elements.functionsWithParams.remove(functionName);
-                controller.remove(functionName);
-                functionName = null;
-            } else {
-                Elements.functions.remove(functionName);
-                Elements.functions.put(nf.getName(), nf.getFunction());
 
-                Elements.functionsWithParams.remove(functionName);
-                Elements.functionsWithParams.put(nf.getName(), nf.getParams());
-
-                controller.remove(functionName);
-                functionName = nf.getName();
-
-                controller.rebuildFunction(functionName);
-                controller.redraw(functionName);
-            }
-            controller.removeTextField();
-            controller.addTextField();
-            controller.addSliders();
-            controller.removeSliders();
-            // System.out.println(Elements.functionsWithParams);
-            // System.out.println(Elements.parameters.keySet());
-        });
+        setOnKeyReleased(keyEvent -> processing(controller));
+        // Необходимо для обновления функций, зависящих от других функций
+        // (кликом, обновляем старую функцию, так как действие не частое, но требовательное для автоматического обновления)
+        setOnMouseClicked(mouseEvent -> processing(controller));
     }
     public TextFieldOnPage(MainController controller){
         this("", controller);
+    }
+    private void processing(MainController controller){
+        String textOfField = getText();
+        NamedFunction nf = Elements.builder.building(textOfField);
+        if(nf == null || (Elements.functions.containsKey(nf.getName()) && !nf.getName().equals(functionName))
+                || (Elements.parameters.containsKey(nf.getName())
+        )) {
+            Elements.functions.remove(functionName);
+            Elements.functionsWithParams.remove(functionName);
+            controller.remove(functionName);
+            functionName = null;
+        } else {
+            Elements.functions.remove(functionName);
+            Elements.functions.put(nf.getName(), nf.getFunction());
+
+            Elements.functionsWithParams.remove(functionName);
+            Elements.functionsWithParams.put(nf.getName(), nf.getParams());
+
+            controller.remove(functionName);
+            functionName = nf.getName();
+
+            controller.rebuildFunction(functionName);
+            controller.redraw(functionName);
+        }
+        controller.removeTextField();
+        controller.addTextField();
+        controller.addSliders();
+        controller.removeSliders();
     }
     private String functionName;
 }
